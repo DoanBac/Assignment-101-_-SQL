a9276a780a8e75ddb9cb615e2cb71d2a0ae30bc9
DoanBac <87013807+DoanBac@users.noreply.github.com>
2025-08-22 14:58:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -18,3 +18,7 @@ def webhook():
     }
     requests.post(telegram_url, json=payload)
     return "OK", 200
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render sẽ cung cấp PORT qua biến môi trường
+    app.run(host='0.0.0.0', port=port)
